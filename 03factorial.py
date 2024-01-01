981dd658ba92a0981990794083f245c11016f70d
Sam <kassit.azizi@gmail.com>
2024-01-01 11:08:39 +0000

    change to factorial

--- 03factorial.py
+++ 03factorial.py
@@ -1,4 +1,4 @@
-def factorial(number):
+""" def factorial(number):
     lst =list(range(1,number+1))
     
     revLst = lst[::-1]
@@ -9,6 +9,24 @@ def factorial(number):
         fact = i
     return fact
    
+
+number = 5
+print(factorial(number)) """
+
+
+def factorial(number):
+    if number < 0 :
+        return None
+    elif number == 1 :
+        return 1
+    else:
+
+        myList = list(range(1,number+1))
+        revList = myList[::-1]
+        fact = 1
+        for i in revList:
+            fact = fact*i
+        return fact
 
 number = 5
 print(factorial(number))
